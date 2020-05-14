0d9bcd7bfd6580f897c51e5eddb8cd7bb7a585c7
NatalliaShulhach <55178199+NatalliaShulhach@users.noreply.github.com>
2020-05-14 16:16:16 +0300

    init: objects-tasks

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* ************************************************************************************************
  *                                                                                                *
  * Plese read the following tutorial before implementing tasks:                                   *
@@ -20,8 +21,13 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const rectagle = {
+    height,
+    width,
+    getArea: height * width,
+  };
+  return rectagle;
 }
 
 
@@ -35,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
